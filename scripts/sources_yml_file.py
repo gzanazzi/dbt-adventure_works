28050843731ed4dc9f85a75d818f5230367fe419
gzanazzi <123099379+gzanazzi@users.noreply.github.com>
2023-02-03 09:35:22 +0100

    Delete sources_yml_file.py

--- scripts/sources_yml_file.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict
-import functions
-import json
-
-def write_source_file(tgt_file: str, model_tables=defaultdict(list)):
-    dbt_version_file = r"2"
-    print("build source yml file: " + tgt_file)
-    print(json.dumps(model_tables, indent = 4))
-    functions.write_source_file_from_TableDef(model_tables, tgt_file,dbt_version_file)
